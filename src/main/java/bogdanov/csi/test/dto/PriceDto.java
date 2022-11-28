dd8a7f8becc07bc156b37c5fa06bfac8ac78be67
dmitrii bogdanov <d.bogdanov@spbu.ru>
2022-11-28 19:43:21 +0300

    - Сделал DTO для цены;

--- src/main/java/bogdanov/csi/test/dto/PriceDto.java
+++ src/main/java/bogdanov/csi/test/dto/PriceDto.java
@@ -1,4 +1,26 @@
 package bogdanov.csi.test.dto;
 
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/** DTO цены продукта */
+@Data
 public class PriceDto {
+
+    /** идентификатор в БД */
+    private long id;
+    /** код товара */
+    private String productCode;
+    /** номер цены */
+    private int number;
+    /** номер отдела */
+    private int depart;
+    /** начало действия */
+    private LocalDateTime begin;
+    /** конец действия */
+    private LocalDateTime end;
+    /** значение цены в копейках */
+    private long value;
+
 }
