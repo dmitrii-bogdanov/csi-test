585b1bf3467b0dc3c365700077d24c5dcd5e9b26
dmitrii bogdanov <d.bogdanov@spbu.ru>
2022-12-01 20:50:57 +0300

    - Заменил тип id на Long;

--- src/main/java/bogdanov/csi/test/dto/PriceDto.java
+++ src/main/java/bogdanov/csi/test/dto/PriceDto.java
@@ -8,10 +8,10 @@ import java.time.LocalDateTime;
 
 /** DTO цены продукта */
 @Data
-public class PriceDto implements Comparable{
+public class PriceDto{
 
     /** идентификатор в БД */
-    private long id;
+    private Long id;
     /** код товара */
     private String productCode;
     /** номер цены */
@@ -25,48 +25,4 @@ public class PriceDto implements Comparable{
     /** значение цены в копейках */
     private long value;
 
-    @Override
-    public int compareTo(Object o) {
-
-        if (o == null) {
-            throw new InvalidPriceException("Object compared against this one is null");
-        }
-
-        final PriceDto other = (PriceDto) o;
-
-        int result;
-
-        result = this.productCode.compareTo(other.productCode);
-        if (result != 0) {
-            return  result;
-        }
-
-        result = Comparators.comparable().compare(this.depart, other.depart);
-        if (result != 0) {
-            return result;
-        }
-
-        result = Comparators.comparable().compare(this.number, other.number);
-        if (result != 0) {
-            return result;
-        }
-
-        result = this.begin.compareTo(other.begin);
-        if (result != 0) {
-            return result;
-        }
-
-        result = this.end.compareTo(other.end);
-        if (result != 0) {
-            return result;
-        }
-
-        result = Comparators.comparable().compare(this.value, other.value);
-        if (result != 0) {
-            return result;
-        }
-
-        return 0;
-
-    }
 }
